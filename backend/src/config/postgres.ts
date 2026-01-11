f98ae3567e81c0df925e59860096dddd525b2925
vivek <codecrafter67@gmail.com>
2026-01-11 21:31:37 +0530

    add commonjs export

--- backend/src/config/postgres.ts
+++ backend/src/config/postgres.ts
@@ -1,11 +1,5 @@
-import  {PrismaClient}  from '@prisma/client';
+const { PrismaClient } = require('@prisma/client');
 
- const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL,
-        },
-    },
-}); 
+const prisma = new PrismaClient();
 
-export default prisma;
+module.exports = prisma;
